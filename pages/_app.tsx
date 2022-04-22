424b3a0468c9e9620bde80f0c5dfa539f59e2f8a
Artem Panko <artem.panko@aimprosoft.com>
2022-04-22 19:08:24 +0300

    resolve TODO

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,6 @@ import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
 import { lightTheme, MeetingProvider } from 'amazon-chime-sdk-component-library-react';
 
-// TODO meeting provider children issue
-
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={lightTheme}>
