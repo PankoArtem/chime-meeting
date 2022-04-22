76f92f15c4cf6c601406538f84ae6ce03c082e75
Artem Panko <artem.panko@aimprosoft.com>
2022-04-22 10:24:46 +0300

    index page UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,69 @@
-import { ChangeEventHandler, useState } from 'react';
+import { ChangeEventHandler, useEffect, useState } from 'react';
 import type { NextPage } from 'next';
 import { useRouter } from 'next/router';
+import {
+  Flex, FormField, Input, PrimaryButton,
+} from 'amazon-chime-sdk-component-library-react';
+import axios from 'axios';
 
 const Home: NextPage = () => {
   const router = useRouter();
 
-  const [id, setId] = useState('');
+  const [meeting, setMeeting] = useState();
+  const [clientName, setClientName] = useState('');
+
+  const [existingMeetingId, setExistingMeetingId] = useState('');
+
+  useEffect(() => {
+    if (meeting) { setExistingMeetingId(meeting.MeetingId); }
+  }, [meeting]);
 
   const handleIdChange:ChangeEventHandler<HTMLInputElement> = (event) => {
     event.preventDefault();
-    setId(event.target.value);
+    setClientName(event.target.value);
   };
 
-  const handleJoinClick = () => {
-    router.push(`/meeting/${id}`);
+  const handleExistingMeetingIdChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+    event.preventDefault();
+    setExistingMeetingId(event.target.value);
+  };
+
+  const handleCreateMeetingClick = async () => {
+    const response = await axios.post(`http://localhost:8080/join?clientId=${clientName}`);
+    const { Info } = response.data;
+    setMeeting(Info.Meeting.Meeting);
+  };
+
+  const handleJoinMeetingClick = () => {
+    router.push(`/meeting/${meeting.MeetingId}`);
   };
 
   return (
-    <div>
-      <label htmlFor="clientId">
-        <p>YOU APPEAR WITH NAME</p>
-        <input type="text" id="clientId" value={id} onChange={handleIdChange} />
-      </label>
-      <button type="button" onClick={handleJoinClick}>Join Meeting</button>
+    <div style={{
+      height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center',
+    }}
+    >
+      <Flex layout="stack">
+        <FormField
+          field={Input}
+          label="How do you want another people call you?"
+          value={clientName}
+          fieldProps={{ name: 'clientName', placeholder: 'Your name' }}
+          onChange={handleIdChange}
+          layout="stack"
+        />
+        <PrimaryButton label="Create Meeting" onClick={handleCreateMeetingClick} />
+        <span style={{ padding: '20px 0', textAlign: 'center' }}>or</span>
+        <FormField
+          field={Input}
+          label="Join your friends with meeting ID"
+          value={existingMeetingId}
+          fieldProps={{ name: 'clientName', placeholder: 'Meeting Id' }}
+          onChange={handleExistingMeetingIdChange}
+          layout="stack"
+        />
+        <PrimaryButton label="Join Existing Meeting" onClick={handleJoinMeetingClick} />
+      </Flex>
     </div>
   );
 };
